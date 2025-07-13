185caba175f1fe47e7049dc02f1f29a69dff94ff
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-07-13 11:09:39 +0530

    Added env.sample file and hidden mongodb uri

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../../constants.js";
 const connectDB=async()=>{
     try{
-         const conn = await mongoose.connect(`mongodb+srv://Shreeshanth99:Shre%40123@animatch.df0o23d.mongodb.net/${DB_NAME}`);
+         const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(err){
         console.log("MongoDb connection failed:",err);
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { regiterUser,loginUser } from "../controllers/authcontroller.js";
 import express from "express"
-// import { Router } from "express";
+
 const router =express.Router()
 router.post("/register",regiterUser);
 router.post("/login",loginUser)
