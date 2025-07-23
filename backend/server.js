8dab67a78b6c558873175a532c22eb90ea682007
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-07-23 12:19:04 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/library", libraryRoutes);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
+app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
 
