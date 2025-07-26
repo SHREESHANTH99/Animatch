7af6eda20518acffbbc9af7d679d9702a930826f
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-07-26 12:59:45 +0530

    changes authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,36 +1,53 @@
 import { regiterUser,loginUser } from "../controllers/authController.js";
 import express from "express";
 import jwt from "jsonwebtoken";
+import { User } from "../models/User.models.js";
 const router =express.Router()
 router.post("/register",regiterUser);
 router.post("/login",loginUser)
 
 router.post('/exchange-token',async(req,res)=>{
     try{
-        const {supabaseToken,userData}= req.body;
-
-        if(!supabaseToken || !userData){
-            return res.status(400).json({error:"Missing supabaseToken or userData"})
+        const {userData}= req.body;
+        let dbUser;
+       try{
+        dbUser=await User.findOne({email:userData.email});
+        if(!dbUser){
+            dbUser=await User.create({
+                email:userData.email,
+                username:userData.user_metadata?.full_name || userData.email,
+                supabaseid:userData.id,
+                createdAt:new Date(),
+            });
+            console.log("Created new user in database:",dbUser._id)
+        }else{
+             console.log("Found existing user in database:",dbUser._id)
         }
-        const customToken = jwt.sign({
-            id:userData.id,
-            email:userData.email,
-            username:userData.user_metadata?.full_name || userData.email,
+       }catch(err){
+        console.log("database error:",err);
+        return res.status(500).json({error:"Database error"})
+       }
+
+       const customToken=jwt.sign(
+        {
+            id:dbUser._id.toString(),
+            email:dbUser.email,
+            username:dbUser.username
         },
         process.env.JWT_SECRET,
-        {expiresIn:'24h'}
-    );
-    res.json({
+        { expiresIn:'24h'}
+       );
+       res.json({
         token:customToken,
         user:{
-            id:userData.id,
-            email:userData.email,
-            username:userData.user_metadata?.full_name || userData.email,
+             id:dbUser._id.toString(),
+            email:dbUser.email,
+            username:dbUser.username
         }
-    })
+       })
     }catch(err){
-        console.error('Token exchange error:',err);
-         return res.status(500).json({error:"Token exchange failed"})
+        console.error("Token exchange error:",err.message);
+            return res.status(500).json({error:err.message})
     }
 })
 export default router;
