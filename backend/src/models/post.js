2bc810cb44b140315f534bbbd53e2a3e830a2169
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-08-23 12:17:33 +0530

    Fixed post

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    content: { type: String, required: true },
+    content: { type: String, required: false },
     username: { type: String, required: true },
     author: {
       type: mongoose.Schema.Types.ObjectId,
